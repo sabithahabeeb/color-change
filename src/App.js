902319b9cc0a2812e8a4115243ac7df1337f1a15
sabithahabeeb <sabithahabeeb.sabi@gmail.com>
2023-10-26 18:28:40 +0530

    color-change

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const handlered = ()=>{
+    // let color = 'red'
+    document.body.style.backgroundColor='red'
+  }
+  const handleblue = ()=>{
+    let color = 'blue'
+    document.body.style.backgroundColor=color
+
+  }
+  const handlegreen = ()=>{
+    let color = 'green'
+    document.body.style.backgroundColor=color
+
+  }
+  const handlepurple = ()=>{
+    let color = 'purple'
+    document.body.style.backgroundColor=color
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <div style={{
+      width:'100%',
+      height:'80vh',
+      display:'flex',
+      flexDirection:'column',
+      justifyContent:'center',
+      alignItems:'center',
+      margin:'50px',
+      padding:'50px',
+
+    }}>
+<h1 style={{color:'black',fontSize:'50px',fontWeight:'600'}}>Background Color Change</h1>
+<div style={{display:'flex',flexDirection:'row'}}>
+  <button onClick={handlered} style={{padding:'15px',margin:'10px',borderRadius:'20px',height:'50px',fontSize:'20px',fontWeight:'600'}}>Red</button>
+  <button onClick={handleblue} style={{padding:'15px',margin:'10px',borderRadius:'20px',height:'50px',fontSize:'20px',fontWeight:'600'}}>Blue</button>
+  <button onClick={handlegreen} style={{padding:'15px',margin:'10px',borderRadius:'20px',height:'50px',fontSize:'20px',fontWeight:'600'}}>Green</button>
+  <button onClick={handlepurple} style={{padding:'15px',margin:'10px',borderRadius:'20px',height:'50px',fontSize:'20px',fontWeight:'600'}}>Purple</button>
+</div>
+</div>
+    </>
   );
 }
 
